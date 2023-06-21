ad70b18e5170b91d78b6739e104163fc11a42da7
alionsss <alionsss@foxmail.com>
2023-06-21 23:42:56 +0800

    调整models

--- models/resnet34.py
+++ models/resnet34.py
@@ -1,3 +1,6 @@
-class ResNet34(object):
+from .BasicModules import BasicModule
+
+
+class ResNet34(BasicModule):
     def __init__(self) -> None:
         super().__init__()
--- models/squeezenet.py
+++ models/squeezenet.py
@@ -1,4 +1,9 @@
-class SqueezeNet(object):
+from .BasicModules import BasicModule
+
+class SqueezeNet(BasicModule):
 
     def __init__(self) -> None:
         super().__init__()
+
+    def printxxx(self):
+        print("xxxxxxxxxxxxxxxxxxxxxxx")
