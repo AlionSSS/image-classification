5e0400180aca9c14a750f94342bce0a976da8efa
alionsss <alionsss@foxmail.com>
2023-06-22 10:55:39 +0800

    添加LeNet

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from .resnet34 import ResNet34
 from .squeezenet import SqueezeNet
+from lenet import LetNet
---
+++ models/lenet.py
@@ -0,0 +1,10 @@
+from .BasicModules import BasicModule
+
+
+class LetNet(BasicModule):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def __forward__(self, X):
+        pass
