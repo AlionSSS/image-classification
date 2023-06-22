3d66398889d59e652a7e22e066a0c7b3654d99b0
alionsss <alionsss@foxmail.com>
2023-06-22 11:28:36 +0800

    添加LeNet

--- models/lenet.py
+++ models/lenet.py
@@ -1,10 +1,26 @@
 from .BasicModules import BasicModule
 
+from torch import nn
+from torch.nn import functional as F
+
 
 class LetNet(BasicModule):
 
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, (5, 5))
+        self.conv2 = nn.Conv2d(6, 16, (5, 5))
+        # self.fc1 = nn.Linear(5 * 5 * 16, 120)
+        self.fc1 = nn.Linear(53 * 53 * 16, 120)
+        self.fc2 = nn.Linear(120, 84)
+        # self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, 2)
 
-    def __forward__(self, X):
-        pass
+    def forward(self, x):
+        out = F.max_pool2d(F.relu(self.conv1(x)), kernel_size=2)  # 110 * 110
+        out = F.max_pool2d(F.relu(self.conv2(out)), kernel_size=2)  # 53 * 53
+        out = out.view(out.size()[0], -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
